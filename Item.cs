6ce42560505e3e5fd7608e91d28135d48c96a103
MingVisional <1307275729@qq.com>
2021-11-26 16:55:53 +0800

    feat: 可拾取物品的属性

---
+++ Item.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new item", menuName = "Inventory/new item")]
+public class Item : ScriptableObject
+{
+    public string ItemName;
+    public int ItemType,TypeNum,ItemCost;
+    public Sprite ItemIcon;
+    [TextArea]
+    public string Infornation;
+}
